8a91a2bb6599d3401922a22b9b5157f65b524029
Ashfame <mail@ashfame.com>
2012-07-31 23:18:09 +0530

    javascript code refactoring

--- engine.js
+++ engine.js
@@ -12,14 +12,14 @@ function enginerun() {
 				response = $.parseJSON(response);
 				if ( response.status == 'success' ) {
 					var dashboard_right_now_tds = $('#dashboard_right_now td');
-					$(dashboard_right_now_tds).eq(0).find('a').html(response.count.posts.publish);
-					$(dashboard_right_now_tds).eq(2).find('a').html(response.count.pages.publish);
-					$(dashboard_right_now_tds).eq(4).find('a').html(response.count.categories);
-					$(dashboard_right_now_tds).eq(6).find('a').html(response.count.tags);
-					$(dashboard_right_now_tds).eq(8).find('a').html(response.count.comments.total_comments);
-					$(dashboard_right_now_tds).eq(10).find('a').html(response.count.comments.approved);
-					$(dashboard_right_now_tds).eq(12).find('a').html(response.count.comments.total_comments-response.count.comments.approved);
-					$(dashboard_right_now_tds).eq(14).find('a').html(response.count.comments.spam);
+					dashboard_right_now_tds.eq(0).find('a').html(response.count.posts.publish);
+					dashboard_right_now_tds.eq(2).find('a').html(response.count.pages.publish);
+					dashboard_right_now_tds.eq(4).find('a').html(response.count.categories);
+					dashboard_right_now_tds.eq(6).find('a').html(response.count.tags);
+					dashboard_right_now_tds.eq(8).find('a').html(response.count.comments.total_comments);
+					dashboard_right_now_tds.eq(10).find('a').html(response.count.comments.approved);
+					dashboard_right_now_tds.eq(12).find('a').html(response.count.comments.total_comments-response.count.comments.approved);
+					dashboard_right_now_tds.eq(14).find('a').html(response.count.comments.spam);
 				}
 			});
 			// Refetch stats after timeout
